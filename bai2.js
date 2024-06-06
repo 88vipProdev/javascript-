3d05c27766a80c6a197e424ff133c53d12d9d34e
88vipProdev <ngiuyentunglam18091998abc@gmail.com>
2024-06-06 08:44:54 +0700

    mang trong js

--- bai2.js
+++ bai2.js
@@ -3,3 +3,8 @@ function writeLog()
     console.log('day la 1 dong log')
 }
 writeLog();
+
+function writeLog2(message){
+    console.log(message)
+}
+writeLog2('test');
---
+++ bai3.js
@@ -0,0 +1,37 @@
+var courses = [
+    {
+        id:1,
+        name:'php',
+        coin :0
+    },
+    {
+        id:2,
+        name:'java',
+        coin: 500
+    },
+    {
+        id:3,
+        name:'c++',
+        coin :500
+
+    },
+    {
+        id :4,
+        name : 'html' ,
+        coin :0
+
+
+
+    },
+    {
+        id : 5 ,
+        name : 'css',
+        coin :0
+    }
+
+
+
+]
+courses.forEach(function(course,index){
+    console.log(course)
+})
