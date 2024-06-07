fd6f7ee3da1acbfa582f722969695847230b3407
88vipProdev <ngiuyentunglam18091998abc@gmail.com>
2024-06-07 15:00:32 +0700

    bai 6

---
+++ bai6.js
@@ -0,0 +1,34 @@
+var courses = [
+    {
+        id:1,
+        nam:'khoa hoc: javascript',
+        coin :500
+    },
+    
+    {
+        id:2,
+        nam:'khoa hoc: javascript',
+        coin :500
+    },
+    {
+        id:3,
+        nam:'c++',
+        coin :500
+    },
+    {
+        id:4,
+        nam:'java',
+        coin :500
+    },
+    {
+        id:5,
+        nam:'ruby',
+        coin :500
+    },
+];
+    function coursesHandle()
+    {
+        
+    }
+var newCourse = courses.map(coursesHandle);
+console.log(newCourse)
