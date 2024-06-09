d0f5083928cf4d1d9c76d5bd4de3ad05657b3aad
88vipProdev <ngiuyentunglam18091998abc@gmail.com>
2024-06-09 14:22:50 +0700

    bai 13

---
+++ bai13.js
@@ -0,0 +1,5 @@
+//  console.log(Math.PI)  ;     //tra ve so PI 
+//  console.log(Math.round(3.5)) // lam tron so 
+//  console.log(Math.abs(-10))  // gia tri tuyet doi
+ console.log(Math.ceil(3.1))
+ console.log(Math.floor(4.9))
