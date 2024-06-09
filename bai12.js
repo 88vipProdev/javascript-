6ac454520c944cdb2a8796d55f91e47c6376868e
88vipProdev <ngiuyentunglam18091998abc@gmail.com>
2024-06-09 14:10:32 +0700

    includes

--- bai12.js
+++ bai12.js
@@ -1,2 +1,8 @@
 var title = 'responsive web design';
-console.log(title.includes("web"));
+console.log(title.includes("responsive" ,1));
+
+
+var courses = ['java','php','c++']
+
+console.log(courses.includes('php' , 2))
+   
